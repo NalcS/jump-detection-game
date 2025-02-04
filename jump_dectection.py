986d22c6d3a9ae1d2c671808a7728a88bb1af719
NalcS <>
2025-02-04 22:52:35 +0100

    Added face detection.

---
+++ jump_dectection.py
@@ -0,0 +1,35 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+# Load the Haar Cascade for face detection
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+scale_factor = 1.5 
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+    
+    height, width = frame.shape[:2]
+    new_width = int(width * scale_factor)
+    new_height = int(height * scale_factor)
+
+    # Resize the frame
+    frame = cv2.resize(frame, (new_width, new_height), interpolation=cv2.INTER_AREA)
+
+    # Detect faces
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=10)
+
+    # Draw rectangles around faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    
+    cv2.imshow("webcam", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
